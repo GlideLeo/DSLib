27fd91512ab937fb5bb90706707b024eb7180f33
JunQiLiu <jassyliu96@gmail.com>
2019-01-31 15:05:33 +0800

    update DynamicArray

--- dynamicarray.h
+++ dynamicarray.h
@@ -12,13 +12,46 @@ class DynamicArray : public Array<T>
 {
 protected:
     int m_length;
-public:
-    DynamicArray(int length)
-    {
-        this -> m_array = new T[length];
 
-        if(this -> m_array != nullptr)
+    T* copy(T* array, int len, int newlen)
+    {
+        T* ret = new T[newlen];
+
+        if(ret != nullptr)
         {
+            int size = (len < newlen) ? len : newlen;
+
+            for(int i = 0; i < size; i++)
+            {
+                ret[i] = array[i];
+            }
+        }
+
+        return ret;
+    }
+
+    void update(T* array, int length)
+    {
+        if(array != nullptr)
+        {
+            T* temp = this -> m_array;
+
+            this -> m_array = array;
+            this -> m_length = length;
+
+            delete [] temp;
+        }
+        else
+        {
+            THROW_EXCEPTION(NoEnoughMemoryException,"No memory to create DynamicArray object ...");
+        }
+    }
+
+    void init(T* array, int length)
+    {
+        if(array != nullptr)
+        {
+            this -> m_array = array;
             this -> m_length = length;
         }
         else
@@ -26,49 +59,68 @@ public:
             THROW_EXCEPTION(NoEnoughMemoryException,"No memory to create DynamicArray object ...");
         }
     }
+public:
+    DynamicArray(int length)
+    {
+        init(new T[length], length);
+//        this -> m_array = new T[length];
+
+//        if(this -> m_array != nullptr)
+//        {
+//            this -> m_length = length;
+//        }
+//        else
+//        {
+//            THROW_EXCEPTION(NoEnoughMemoryException,"No memory to create DynamicArray object ...");
+//        }
+    }
 
     DynamicArray(const DynamicArray<T>& obj)
     {
-        this -> m_array = new T[obj.m_length];
+//        T* array =  copy(obj.m_array, obj.m_length, obj.m_length);
+        init(copy(obj.m_array, obj.m_length, obj.m_length), obj.m_length);
+//        this -> m_array = new T[obj.m_length];
 
-        if(this -> m_array != nullptr)
-        {
-            this -> m_length = obj.m_length;
+//        if(this -> m_array != nullptr)
+//        {
+//            this -> m_length = obj.m_length;
 
-            for(int i = 0; i < obj.m_length; i++)
-            {
-                this->m_array[i] = obj.m_array[i];
-            }
-        }
-        else
-        {
-            THROW_EXCEPTION(NoEnoughMemoryException,"No memory to create DynamicArray object ...");
-        }
+//            for(int i = 0; i < obj.m_length; i++)
+//            {
+//                this->m_array[i] = obj.m_array[i];
+//            }
+//        }
+//        else
+//        {
+//            THROW_EXCEPTION(NoEnoughMemoryException,"No memory to create DynamicArray object ...");
+//        }
     }
     DynamicArray<T>& operator= (const DynamicArray<T>& obj)
     {
         if(this != &obj)
         {
-            T* array = new T[obj.m_length];
+            //T* array = new T[obj.m_length];
+            //T* array = copy(obj.m_array, obj.m_length, obj.m_length);
+            update(copy(obj.m_array, obj.m_length, obj.m_length), obj.m_length);
 
-            if(array != nullptr)
-            {
-                for(int i = 0; i < obj.m_length; i++)
-                {
-                    array[i] = obj.m_array[i];
-                }
+//            if(array != nullptr)
+//            {
+//                for(int i = 0; i < obj.m_length; i++)
+//                {
+//                    array[i] = obj.m_array[i];
+//                }
 
-                T* temp = this -> m_array;
+//                T* temp = this -> m_array;
 
-                this -> m_array = array;
-                this -> m_length = obj.m_length;
+//                this -> m_array = array;
+//                this -> m_length = obj.m_length;
 
-                delete [] temp;
-            }
-            else
-            {
-                THROW_EXCEPTION(NoEnoughMemoryException,"No memory to create DynamicArray object ...");
-            }
+//                delete [] temp;
+//            }
+//            else
+//            {
+//                THROW_EXCEPTION(NoEnoughMemoryException,"No memory to create DynamicArray object ...");
+//            }
         }
         return *this;
     }
@@ -79,29 +131,32 @@ public:
     }
     void resize(int length)
     {
-        if(length != this->m_length)
+        if(length != m_length)
         {
-            T* array = new T[length];
-            if(array != nullptr)
-            {
-                int size = (length < m_length) ? length : m_length;
+//            T* array = copy(this->m_array, m_length, length);
+            update(copy(this->m_array, m_length, length), length);
+//            T* array = new T[length];
+//            if(array != nullptr)
+//            {
+//                int size = (length < m_length) ? length : m_length;
 
-                for(int i = 0; i < size; i++)
-                {
-                    array[i] = this -> m_array[i];
-                }
 
-                T* temp = this -> m_array;
+//                for(int i = 0; i < size; i++)
+//                {
+//                    array[i] = this -> m_array[i];
+//                }
 
-                this -> m_length = length;
-                this -> m_array = array;
+//                T* temp = this -> m_array;
 
-                delete [] temp;
-            }
-            else
-            {
-                THROW_EXCEPTION(NoEnoughMemoryException,"No memory to create DynamicArray object ...");
-            }
+//                this -> m_length = length;
+//                this -> m_array = array;
+
+//                delete [] temp;
+//            }
+//            else
+//            {
+//                THROW_EXCEPTION(NoEnoughMemoryException,"No memory to create DynamicArray object ...");
+//            }
         }
     }
 
